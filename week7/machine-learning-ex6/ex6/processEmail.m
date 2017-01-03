887bae75b9f839288b07a1632b76eb1ca65512f0
Liu Binsheng <liubinsheng@gmail.com>
2017-01-03 20:23:41 +0800

    complete part 2

--- week7/machine-learning-ex6/ex6/processEmail.m
+++ week7/machine-learning-ex6/ex6/processEmail.m
@@ -97,15 +97,11 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
 
-
-
-
-
-
-
-
-
-
+    for i = 1:length(vocabList)
+      if 1 == strcmp(str, vocabList{i})
+	word_indices = [word_indices; i];
+      endif
+    endfor
     % =============================================================
 
 
